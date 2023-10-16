c2823093e93544d97dfcd6324ff4d208d4998f9c
Leticia Araujo <51300153+araujoleticiafb@users.noreply.github.com>
2023-10-16 16:19:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-/usr/bin/java -jar /var/wiremock/lib/wiremock-jre8-standalone.jar --global-response-templating=true
+set -e
+
+# Set `java` command if needed
+if [ "$1" = "" -o "${1#-}" != "$1" ]; then
+  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run "$@" $WIREMOCK_OPTIONS
+fi
+
+# allow the container to be started with `-e uid=`
+if [ "$uid" != "" ]; then
+  # Change the ownership of /home/wiremock to $uid
+  chown -R $uid:$uid /home/wiremock
+
+  set -- su-exec $uid:$uid "$@"  $WIREMOCK_OPTIONS
+fi
+
+exec "$@"  $WIREMOCK_OPTIONS
